14cc1c7188c1a3f682aac2cb2462e9231a07b170
Piyush Mane <piyushmanekarad123@gmail.com>
2026-06-30 08:22:15 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask,request,redirect,url_for,render_template 
+import requests
+
+app = Flask(__name__) 
+
+@app.route("/", methods=["GET","POST"])
+def home():
+  
+    if request.method=="POST":
+
+        city = request.form.get("city")
+        api_key = "e1ec8cb28a274595a7c64648262906 "
+        url = f"http://api.weatherapi.com/v1/current.json?key={api_key}&q={city}"
+        response = requests.get(url)
+        if city:
+            data = response.json()
+
+        # print(f"City:{data["location"]["name"]}\nTemprature:{data["current"]["temp_c"]}°C|{data["current"]["condition"]["text"]}\nHumidity:{data["current"]["humidity"]}")
+
+        return render_template("index.html",city=data["location"]["name"],temperature=data["current"]["temp_c"],
+                            condition=data["current"]["condition"]["text"],
+                            humidity=data["current"]["humidity"]) 
+    return render_template("index.html")
+
+    
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
